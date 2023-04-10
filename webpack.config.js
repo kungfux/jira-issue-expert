404ee456e308e4238af025a55c6dc9f71fb6153b
Alexander Fuks <kungfux@users.noreply.github.com>
2023-04-10 13:04:18 +0400

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        './src/manifest.json',
+        { from: './src/icons', to: 'icons' }
+      ],
+    }),
+  ],
+};
